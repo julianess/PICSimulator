4622f742c218296fb9d89ead8aeaf439a2a5e817
DenKu <dennis.kuebel@t-online.de>
2015-04-09 14:15:48 +0200

    File Picker mit Umlauten
    Cancel noch implementieren

--- src/simulator/controller/SingleLayoutController.java
+++ src/simulator/controller/SingleLayoutController.java
@@ -1,25 +1,16 @@
 package simulator.controller;
 
-import java.awt.FileDialog;
-import java.awt.Frame;
-import java.io.File;
+
 import java.io.IOException;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import javax.swing.JFileChooser;
-
-import application.Main;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -34,31 +25,36 @@ public class SingleLayoutController {
 	@FXML
 	private TextArea editor;
 
-	private AnchorPane singleLayout;
-
 	@FXML
 	public void onClickOpenFile() {
 
+		//Variablen
+		
+		//Inhaltsstring fuer das Textfenster
+		String Inhalt = "";
+		
+		//Charset fuer Umlaute
+		Charset charset = Charset.forName("ISO-8859-1");
+		
 		FileChooser fileChooser = new FileChooser();
 
 		Stage primaryStage = (Stage) editor.getScene().getWindow();
 
 		fileChooser.setTitle("Choose a File");
 		Path file = Paths.get(fileChooser.showOpenDialog(primaryStage).getPath());
-		String Inhalt = "";
 
 		try {
-			List<String> lines = Files.readAllLines(file);
-			for (String line : lines) {
-				Inhalt += line + "\n"; // F�gt nach jeder eingelesenen Zeile
-										// einen Zeilenumbruch ein
+			List<String> lines = Files.readAllLines(file, charset);
+			for (String line : lines)
+			{
+				//Fuegt nach jeder eingelesenen Zeile einen Zeilenumbruch ein
+				Inhalt += line + "\n";
 			}
 			// Entfernt den Zeilenumbruch nach der letzten Zeile
 			Inhalt = Inhalt.substring(0, Inhalt.length() - 1); 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		editor.setText(Inhalt);
 	}
 }
