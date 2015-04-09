1f47ef956c0a06830b861c467c75da4ed2805cbc
DenKu <dennis.kuebel@t-online.de>
2015-04-09 12:26:54 +0200

    FileChooser mit Text Implementierung
    Muss noch auf Abbrechen abgefragt werden

--- src/simulator/controller/SingleLayoutController.java
+++ src/simulator/controller/SingleLayoutController.java
@@ -24,56 +24,41 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class SingleLayoutController {
-	
-	
-	
+
 	@FXML
 	private MenuItem openFile;
-	
+
 	@FXML
 	private MenuItem closeFile;
-	
+
 	@FXML
 	private TextArea editor;
-	
+
 	private AnchorPane singleLayout;
-	
+
 	@FXML
-	public void onClickOpenFile(){
-		//String file = "C:\\Users\\Dennis\\Desktop\\Test.txt";
-	
-		
-		//JFileChooser chooser = new JFileChooser();
+	public void onClickOpenFile() {
+
 		FileChooser fileChooser = new FileChooser();
-		
+
 		Stage primaryStage = (Stage) editor.getScene().getWindow();
-		
+
 		fileChooser.setTitle("Choose a File");
-		fileChooser.showOpenDialog(primaryStage);
-		
-		
-		//int rueckgabeWert = chooser.showOpenDialog(null);
-        
-        /* Abfrage, ob auf "�ffnen" geklickt wurde */
-        /* if(rueckgabeWert == JFileChooser.APPROVE_OPTION)
-        {
-             // Ausgabe der ausgewaehlten Datei
-             File = chooser.getSelectedFile().getPath();
-           try
-           {
-                 List<String> lines = Files.readAllLines(Paths.get(File), Charset.defaultCharset());
-                 for (String line : lines)
-                 {
-                     Inhalt += line + "\n"; //F�gt nach jeder eingelesenen Zeile einen Zeilenumbruch ein
-                 }
-                 Inhalt = Inhalt.substring(0, Inhalt.length()-1); //Entfernt den Zeilenumbruch nach der letzten Zeile
-            } 
-     		catch (IOException e)
-     		{
-                 e.printStackTrace();
-             }
-     		
-     		editor.setText(Inhalt); 
-        } */ 
+		Path file = Paths.get(fileChooser.showOpenDialog(primaryStage).getPath());
+		String Inhalt = "";
+
+		try {
+			List<String> lines = Files.readAllLines(file);
+			for (String line : lines) {
+				Inhalt += line + "\n"; // F�gt nach jeder eingelesenen Zeile
+										// einen Zeilenumbruch ein
+			}
+			// Entfernt den Zeilenumbruch nach der letzten Zeile
+			Inhalt = Inhalt.substring(0, Inhalt.length() - 1); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		editor.setText(Inhalt);
 	}
 }
