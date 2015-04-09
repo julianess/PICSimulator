f80eb8826289799eb7469c2494c5923f6a513f69
julianess <julian.ess@gmx.de>
2015-04-09 12:07:47 +0200

    Introducing new FileChooser
    fuck this shit: JFileChooser

--- src/simulator/controller/SingleLayoutController.java
+++ src/simulator/controller/SingleLayoutController.java
@@ -1,5 +1,7 @@
 package simulator.controller;
 
+import java.awt.FileDialog;
+import java.awt.Frame;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -11,11 +13,19 @@ import java.util.List;
 
 import javax.swing.JFileChooser;
 
+import application.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class SingleLayoutController {
+	
+	
 	
 	@FXML
 	private MenuItem openFile;
@@ -26,17 +36,26 @@ public class SingleLayoutController {
 	@FXML
 	private TextArea editor;
 	
+	private AnchorPane singleLayout;
+	
 	@FXML
 	public void onClickOpenFile(){
 		//String file = "C:\\Users\\Dennis\\Desktop\\Test.txt";
-		String Inhalt = "";
-		String File;
+	
 		
-		JFileChooser chooser = new JFileChooser();
-		int rueckgabeWert = chooser.showOpenDialog(null);
+		//JFileChooser chooser = new JFileChooser();
+		FileChooser fileChooser = new FileChooser();
+		
+		Stage primaryStage = (Stage) editor.getScene().getWindow();
+		
+		fileChooser.setTitle("Choose a File");
+		fileChooser.showOpenDialog(primaryStage);
+		
+		
+		//int rueckgabeWert = chooser.showOpenDialog(null);
         
-        /* Abfrage, ob auf "Öffnen" geklickt wurde */
-        if(rueckgabeWert == JFileChooser.APPROVE_OPTION)
+        /* Abfrage, ob auf "ï¿½ffnen" geklickt wurde */
+        /* if(rueckgabeWert == JFileChooser.APPROVE_OPTION)
         {
              // Ausgabe der ausgewaehlten Datei
              File = chooser.getSelectedFile().getPath();
@@ -45,7 +64,7 @@ public class SingleLayoutController {
                  List<String> lines = Files.readAllLines(Paths.get(File), Charset.defaultCharset());
                  for (String line : lines)
                  {
-                     Inhalt += line + "\n"; //Fügt nach jeder eingelesenen Zeile einen Zeilenumbruch ein
+                     Inhalt += line + "\n"; //Fï¿½gt nach jeder eingelesenen Zeile einen Zeilenumbruch ein
                  }
                  Inhalt = Inhalt.substring(0, Inhalt.length()-1); //Entfernt den Zeilenumbruch nach der letzten Zeile
             } 
@@ -54,7 +73,7 @@ public class SingleLayoutController {
                  e.printStackTrace();
              }
      		
-     		editor.setText(Inhalt);
-        }
+     		editor.setText(Inhalt); 
+        } */ 
 	}
 }
