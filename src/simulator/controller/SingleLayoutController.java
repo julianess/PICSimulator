c5270e6e178074460651d803b2c2b83d719b5e80
DenKu <dennis.kuebel@t-online.de>
2015-05-11 17:10:08 +0200

    Sleep Intervall
    Intervall geaendert
    
    TODO:
    Timer Interrupt dbuggen

--- src/simulator/controller/SingleLayoutController.java
+++ src/simulator/controller/SingleLayoutController.java
@@ -545,7 +545,7 @@ public class SingleLayoutController {
 						decoder.decode(test);
 						
 						try {
-							Thread.sleep(10);
+							Thread.sleep(50);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
